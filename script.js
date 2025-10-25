955369708ebee691b52645d3b12d3af38c29e7ec
programer10007 <107678005+SabhyaAggarwal@users.noreply.github.com>
2025-10-25 19:34:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+/*
+Run this model in Javascript
+
+> npm install openai
+*/
+import OpenAI from "openai";
+
+// To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings. 
+// Create your PAT token by following instructions here: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+const token = process.env["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"];
+
+export async function main() {
+
+  const client = new OpenAI({
+    baseURL: "https://models.github.ai/inference",
+    apiKey: token
+  });
+
+  const response = await client.chat.completions.create({
+    messages: [
+      { role:"system", content: "" },
+      { role:"user", content: "What is the capital of France?" }
+    ],
+    model: "openai/gpt-4o",
+    temperature: 1,
+    max_tokens: 4096,
+    top_p: 1
+  });
+
+  console.log(response.choices[0].message.content);
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
